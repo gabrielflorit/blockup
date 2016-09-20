1291b9da38062bc1991b42bda3bc11e34575598d
Gabriel Florit <gabrielflorit@gmail.com>
2016-09-20 14:37:21 -0400

    Add blockup new and blockup serve

--- index.js
+++ index.js
@@ -1,29 +1,80 @@
 #!/usr/bin/env node
+
 var fs = require('fs-extra')
 var path = require('path')
 var bs = require('browser-sync').create()
 var buble = require('buble')
+var chalk = require('chalk')
 
-// Copy files to destination directory.
-fs.copySync(path.join(__dirname, 'src'), path.join(process.cwd()))
+var argv = require('yargs')
+	.usage('Usage: $0 <command> [options]')
+	.command('new', 'setup a new block')
+	.command('serve', 'serve current block')
 
-// Watch index.html and reload.
-bs.watch(path.join(process.cwd(), 'index.html')).on('change', bs.reload)
+	.alias('f', 'file')
+	.nargs('f', 1)
+	.describe('f', 'Load a file')
+	.demand(1, ['f'])
 
-// Watch index.es6.js, compile, and reload.
-bs.watch(path.join(process.cwd(), 'index.js'), function (event, file) {
-  if (event === 'change') {
-    var contents = fs.readFileSync(file, { encoding: 'utf8' })
-    var output = buble.transform(contents)
-    fs.writeFileSync(path.join(process.cwd(), 'dist.js'), output.code)
-    bs.reload()
-  }
-})
+	.help('h')
+	.alias('h', 'help')
+	.argv
 
-bs.init({
-  open: false,
-  server: {
-    baseDir: process.cwd(),
-    index: 'index.html'
-  }
-})
+const newBlock = () => {
+
+	console.log(chalk.green('Scaffolding a new block... done.'))
+
+	// Copy files to destination directory.
+	fs.copySync(path.join(__dirname, 'src'), path.join(process.cwd()))
+
+}
+
+const serveBlock = () => {
+
+	console.log(chalk.green('Serving current block:'))
+
+	// Watch index.html and reload.
+	bs.watch(path.join(process.cwd(), 'index.html')).on('change', bs.reload)
+
+	// Watch index.es6.js, compile, and reload.
+	bs.watch(path.join(process.cwd(), 'index.js'), function (event, file) {
+		if (event === 'change') {
+			var contents = fs.readFileSync(file, { encoding: 'utf8' })
+			var output = buble.transform(contents)
+			fs.writeFileSync(path.join(process.cwd(), 'dist.js'), output.code)
+			bs.reload()
+		}
+	})
+
+	bs.init({
+		open: false,
+		server: {
+			baseDir: process.cwd(),
+			index: 'index.html'
+		}
+	})
+
+}
+
+var command = argv._[0]
+
+switch (command) {
+
+	case 'new': {
+
+		newBlock()
+		serveBlock()
+		break
+
+	}
+
+	case 'serve': {
+
+		serveBlock()
+		break
+
+	}
+
+	default:
+		break
+}
