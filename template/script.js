78951f408a9ff483c2eb13bcfc02905e671f9182
Gabriel Florit <gabrielflorit@gmail.com>
2018-05-24 17:40:53 -0400

    Switch to 2-space instead of tabs

--- template/script.js
+++ template/script.js
@@ -2,9 +2,10 @@ const margin = { top: 10, right: 30, bottom: 30, left: 30 }
 const width = 960 - margin.left - margin.right
 const height = 500 - margin.top - margin.bottom
 
-const svg = d3.select('body').append('svg')
-	.attr('width', width + margin.left + margin.right)
-	.attr('height', height + margin.top + margin.bottom)
+const svg = d3
+  .select('body')
+  .append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
 
-const g = svg.append('g')
-	.attr('transform', `translate(${margin.left}, ${margin.top})`)
+svg.append('g').attr('transform', `translate(${margin.left}, ${margin.top})`)
